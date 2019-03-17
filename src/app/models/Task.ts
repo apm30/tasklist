3077f10454be9cb9982640930b79c8507c52a350
Domo <stolarczyk.dominik@gmail.com>
2019-03-17 15:40:32 +0100

    +Tasks component

---
+++ src/app/models/Task.ts
@@ -0,0 +1,6 @@
+export class Task {
+  name: string;
+  description: string;
+  owner: string;
+  completed: boolean;
+}
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,34 @@
+import { Task } from './../models/Task';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[];
+
+  ngOnInit(): void {
+    this.tasks = [
+      {
+        name: 'name 1',
+        description: 'description 1',
+        owner: 'owner 1',
+        completed: false
+      },
+      {
+        name: 'name 2',
+        description: 'description 2',
+        owner: 'owner 2',
+        completed: false
+      },
+      {
+        name: 'name 3',
+        description: 'description 3',
+        owner: 'owner 3',
+        completed: false
+      }
+    ];
+  }
+}
